b3eaa0d24f6a3890f778476ffc82f64cfd84f1fa
basusaswata <basu.saswata@gmail.com>
2018-06-10 14:52:32 -0500

    1

--- src/main/java/com/tw/mc/mem/CustomerService/controller/CustomerController.java
+++ src/main/java/com/tw/mc/mem/CustomerService/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.tw.mc.mem.CustomerService.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tw.mc.mem.CustomerService.model.Customer;
 
 @RestController
+@RefreshScope
 public class CustomerController {
 
+	 @Value("${user.role}")
+	  private String role;
 	public CustomerController() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	@RequestMapping(value ="/customers/{customerId}" , method = RequestMethod.GET)
     public Customer getCUstomer(@PathVariable("customerId") int customerId) {
-        return new Customer(1,"sas","bas","stage");
+		
+        return new Customer(1,"sas","bas",role);
     }
 
 }
